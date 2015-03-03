8a7dfb827821fcf565a283cc45c2739931b34359
Frikkie Snyman <frikkie.snyman@live.co.za>
2015-03-03 15:39:02 +0200

    More playing around and makefile and stuffs

---
+++ code/headers/test.h
@@ -0,0 +1,9 @@
+#ifndef _TEST_CLASS
+#define _TEST_CLASS
+
+class Test{
+	public:
+		void foo();
+};
+
+#endif
--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include "headers/test.h"
 
 using namespace std;
 int main(){
-	cout << "Hello world" << endl;
+	Test test;
+	test.foo();
+	// cout << "Hello world" << endl;
 }
